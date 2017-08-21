7665f5fdd79d8cd5a70e798aadd6ba40779799a8
michal kudrnac <michalkud@gmail.com>
2017-08-21 13:25:55 +0200

    contact detail update

--- src/components/ContactDetail/ContactDetail.js
+++ src/components/ContactDetail/ContactDetail.js
@@ -15,17 +15,66 @@ const propTypes = {
 
 class ContactDetail extends Component {
 
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = props;
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    componentDidMount() {
+
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('A name was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        console.log(this.props);
         return (
             <Layout>
                 <Content>
-                    {this.props.name}  
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name:
+                            <input type="text" value={this.props.name} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Phone:
+                            <input type="text" value={this.props.phone} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Address:
+                            <input type="text" value={this.props.address} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Email:
+                            <input type="text" value={this.props.email} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Birthday:
+                            <input type="text" value={this.props.birthday} onChange={this.handleChange} />
+                        </label>
+                        <label>
+                            Web:
+                            <input type="text" value={this.props.web} onChange={this.handleChange} />
+                        </label>
+                        <input type="submit" value="Submit" />
+                        
+                    </form>  
+                    {/* {this.props.name}  
                     {this.props.phone}
                     {this.props.address}
                     {this.props.email}
                     {this.props.web}
-                    {this.props.birthday}
+                    {this.props.birthday} */}
                 </Content>
             </Layout>
         );
